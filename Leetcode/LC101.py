a741ee71b88e57448cf914de527333c4b3411fcb
jasper-jiang <jiangweiqi0309@163.com>
2020-10-21 18:53:23 +0800

    add

--- Leetcode/LC101.py
+++ Leetcode/LC101.py
@@ -1,18 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+recursively 的解法，不太容易想，但也不至于很难
+"""
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+
+        def helper(node1, node2):
+            if not node1 and not node2:
+                return True
+            if (not node1 and node2) or (not node2 and node1):
+                return False
+            if node1.val != node2.val:
+                return False 
+
+            return helper(node1.left, node2.right) and helper(node1.right, node2.left)
+        return helper(root,root)
+
+"""
+iteratively 的思路，和recursively 其实差不多的思想
+"""
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         
-        return self.helper(root,root)
-        
-        
-    def helper(self, p,q):
-        if not q and p:
-            return False
-        if q and not p:
-            return False
-        if not q and not p:
-            return True
-        if q and p:
-            if q.val == p.val:
-                return self.helper(p.right,q.left) and self.helper(p.left,q.right)
-            else:
+        candidate = [(root,root)]
+        while candidate:
+            p,q = candidate.pop(0)
+            if (not p and q) or (not q and p):
+                return False 
+            if p and q and p.val!=q.val:
                 return False
+            if p and q:
+                candidate.append((p.left, q.right))
+                candidate.append((p.right, q.left))
+        return True
--- Leetcode/LC98.py
+++ Leetcode/LC98.py
@@ -1,17 +1,48 @@
+"""
+recursively 的思路，重点在于需要更新low 和high， 不能简单的判断当前node和子节点是否满足BST，还要考虑上一级
+"""
+class Solution:
+	def isValidBST(self, root: TreeNode) -> bool:
+		def helper(self,low,high,root):
+			if not root:
+				return True
+			if root.val<=low or root.val>=high:
+				return False
+			return self.helper(low,root.val,root.left) and self.helper(root.val,high,root.right)
+		
+		low = -float('inf')
+		high = float('inf')
+		return self.helper(low,high,root)
+		
+		
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+BST 的中序遍历结果是ordered， i<i+1 如果出现任意i>=i+1则bst不成立
+"""
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         
-        low = -float('inf')
-        high = float('inf')
+        def inorder(root,vals):
+            if not root:
+                return 
+            inorder(root.left, vals)
+            vals.append(root.val)
+            inorder(root.right, vals)
         
-        return self.helper(low,high,root)    
+        def judge(vals):
+            if len(vals) <=1:
+                return True
+            for i in range(len(vals)-1):
+                if vals[i]>=vals[i+1]:
+                    return False 
+            return True 
         
-        
-        
-    def helper(self,low,high,root):
-        if not root:
-            return True
-        
-        if root.val<=low or root.val>=high:
-            return False
-        return self.helper(low,root.val,root.left) and self.helper(root.val,high,root.right)
+        vals = []
+        inorder(root,vals)
+        return judge(vals)
+                
